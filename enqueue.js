d13c497e6080d70002144f0ec8375b4f56c26499
kchoo <35017578+kchoo@users.noreply.github.com>
2018-01-13 12:17:35 -0600

    Create enqueue.js

---
+++ enqueue.js
@@ -0,0 +1,25 @@
+const sqs = new (require('aws-sdk').SQS)();
+
+const $q = require('kchoo-q');
+
+const sqsGetQueueUrlQ = $q.promisify(sqs.getQueueUrl, sqs);
+const sqsSendMessageQ = $q.promisify(sqs.sendMessage, sqs);
+
+const queuePrefix = 'kchoo-media-aggregator';
+
+module.exports = function (name, ...message) {
+	return function (...error) {
+		return sqsGetQueueUrlQ({
+				QueueName: `${queuePrefix}-${name}`
+			}).
+			then(function ({QueueUrl}) {
+				return sqsSendMessageQ({
+					MessageBody: JSON.stringify(message.concat(error)),
+					QueueUrl
+				});
+			}).
+			then(function () {
+				return message;
+			});
+	};
+};
