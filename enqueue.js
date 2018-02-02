1d324637d175c6df2f783262e33d9b1a15bc4db0
kchoo <kchoodev@gmail.com>
2018-02-01 22:57:21 -0600

    Update enqueue.js

--- enqueue.js
+++ enqueue.js
@@ -1,26 +1,33 @@
-const sqs = new (require('aws-sdk').SQS)();
+const sqs = new (require('aws-sdk').SQS)(require('../config.json').aws);
 
 const $q = require('kchoo-q');
 
 const sqsGetQueueUrlQ = $q.promisify(sqs.getQueueUrl, sqs);
 const sqsSendMessageQ = $q.promisify(sqs.sendMessage, sqs);
+const sqsCreateQueueQ = $q.promisify(sqs.createQueue, sqs);
 
 const queuePrefix = 'kchoo-media-aggregator';
 
 module.exports = function (success, name, ...message) {
 	return function (...error) {
-		return sqsGetQueueUrlQ({
-				QueueName: `${queuePrefix}-${name}`
+		const fullMessage = message.concat(error);
+		const QueueName = `${queuePrefix}-${name}`;
+		return sqsGetQueueUrlQ({QueueName}).
+			catch(function (res) {
+				if (res.code === 'AWS.SimpleQueueService.NonExistentQueue') {
+					return sqsCreateQueueQ({QueueName});
+				}
+				throw res;
 			}).
 			then(function ({QueueUrl}) {
 				return sqsSendMessageQ({
-					MessageBody: JSON.stringify(message.concat(error)),
+					MessageBody: JSON.stringify(fullMessage),
 					QueueUrl
 				});
 			}).
 			then(function () {
 				if (!success) {
-					throw message;
+					throw new Error(fullMessage);
 				}
 			});
 	};
